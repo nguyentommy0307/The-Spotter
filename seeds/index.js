0b385bcfcd2db275d853670bfc455d28e0a555c3
nguyentommy0307 <nguyentommy315@g.ucla.edu>
2025-09-13 21:34:38 -0700

    Added images and styled home page

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await spotter.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * cities.length);
+        const price = Math.floor(Math.random() * 20) + 10
         const gymSpot = new spotter({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(places)} ${sample(descriptors)}`
+            title: `${sample(places)} ${sample(descriptors)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Gym Photos',
+            price
         })
         await gymSpot.save();
     }

1a36053f8786c83ff4e0efe5cdab799e70efb113
nguyentommy0307 <nguyentommy315@g.ucla.edu>
2025-09-16 00:55:03 -0700

    Added validation for new form inputs/edits and added error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync')
 const methodOverride = require('method-override');
 const spotter = require('./models/spotter');
 
@@ -27,41 +28,47 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/spotters', async (req, res) => {
+app.get('/spotters', catchAsync(async (req, res) => {
     const spotters = await spotter.find({});
     res.render('spotters/index', { spotters })
-})
+}))
 
 app.get('/spotters/new', (req, res) => {
     res.render('spotters/new')
 })
 
-app.post('/spotters', async (req, res) => {
+app.post('/spotters', catchAsync(async (req, res, next) => {
+
     const GymSpot = new spotter(req.body.spotter);
     await GymSpot.save();
     res.redirect(`/spotters/${GymSpot._id}`)
-})
 
-app.get('/spotters/:id', async (req, res) => {
+}))
+
+app.get('/spotters/:id', catchAsync(async (req, res) => {
     const spotters = await spotter.findById(req.params.id)
     res.render('spotters/show', { spotters })
-})
+}))
 
-app.get('/spotters/:id/edit', async (req, res) => {
+app.get('/spotters/:id/edit', catchAsync(async (req, res) => {
     const spotters = await spotter.findById(req.params.id)
     res.render('spotters/edit', { spotters })
-})
+}))
 
-app.put('/spotters/:id', async (req, res) => {
+app.put('/spotters/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const Gymspot = await spotter.findByIdAndUpdate(id, { ...req.body.spotter })
     res.redirect(`/spotters/${Gymspot._id}`)
-});
+}));
 
-app.delete('/spotters/:id', async (req, res) => {
+app.delete('/spotters/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await spotter.findByIdAndDelete(id);
     res.redirect('/spotters');
+}))
+
+app.use((err, req, res, next) => {
+    res.send('Error! Something has gone wrong.')
 })
 
 app.listen(3000, () => {

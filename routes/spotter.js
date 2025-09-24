c25b010f26d70c81f074162f2edb76f155ab11ad
nguyentommy0307 <nguyentommy315@g.ucla.edu>
2025-09-24 00:13:52 -0700

    created login, register, and logout functionalities

--- routes/spotter.js
+++ routes/spotter.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { spotterSchema } = require('../schemas.js')
+const { isLoggedIn } = require('../middleware')
 const ExpressError = require('../utils/ExpressError');
 const spotter = require('../models/spotter')
+
 
 const validateSpotter = (req, res, next) => {
     const { error } = spotterSchema.validate(req.body);
@@ -21,11 +23,13 @@ router.get('/', catchAsync(async (req, res) => {
     res.render('spotters/index', { spotters })
 }))
 
-router.get('/new', (req, res) => {
-    res.render('spotters/new')
+
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('spotters/new');
 })
 
-router.post('/', validateSpotter, catchAsync(async (req, res, next) => {
+
+router.post('/', isLoggedIn, validateSpotter, catchAsync(async (req, res, next) => {
     const GymSpot = new spotter(req.body.spotter);
     await GymSpot.save();
     req.flash('success', 'Successfully put in a new gym!');
@@ -41,7 +45,7 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('spotters/show', { spotters })
 }))
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const spotters = await spotter.findById(req.params.id);
     if (!spotters) {
         req.flash('error', 'Cannot find that gym');
@@ -50,14 +54,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('spotters/edit', { spotters });
 }))
 
-router.put('/:id', validateSpotter, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateSpotter, catchAsync(async (req, res) => {
     const { id } = req.params;
     const Gymspot = await spotter.findByIdAndUpdate(id, { ...req.body.spotter });
     req.flash('success', 'Successfully updated gym information')
     res.redirect(`/spotters/${Gymspot._id}`)
 }))
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await spotter.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted gym')
